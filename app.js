fc5b4ada84d0b737ba681c03e3ca442bf61903fe
nicholas-stoll22 <nicholascourselstoll@gmail.com>
2024-05-03 20:45:40 -0500

    Full functionality implemented. Just need to add auto board reset.

--- app.js
+++ app.js
@@ -12,5 +12,29 @@ function sketchBoard(size) {
       screen.appendChild(col);
     }
   }
-  
-  sketchBoard(16);
+
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("open");
+});
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const data = Object.fromEntries(new FormData(e.target).entries());
+  userGenerate(data);
+});
+
+function userGenerate(data){
+  let gridSize = data["val"]
+  if (gridSize > 100) {
+    console.log("too big")
+  } else
+  sketchBoard(gridSize)
+}
